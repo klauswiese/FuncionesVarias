abee93d9df07c668756bd8d7acc81dd63cc58784
Hondunauta <klauswiese@users.noreply.github.com>
2021-06-29 20:56:36 -0600

    Update 09_Validacion.R

--- ClasificadorUsoSuelo/09_Validacion.R
+++ ClasificadorUsoSuelo/09_Validacion.R
@@ -3,8 +3,6 @@
 #---------------------------------------------------------
 
 #definir dirección de trabajo
-setwd("~/R/0_Linares/")
-
 #Libraries
 library(sf)
 library(raster)
@@ -16,22 +14,41 @@ library(dplyr)
 MET_sf <- st_read("SHP/RF7clean5000/RF7clean5000.shp")
 MET_sf <- filter(MET_sf, DN != -2147483648)
 
-PuntosVal <- st_transform(st_read("SHP/0_Validacion/RabdomSample1000.gpkg.shp"), crs=crs(MET_sf))
+PuntosVal <- st_transform(st_read("SHP/0_Validacion/RabdomSample1000.gpkg.shp"), 
+                          crs=crs(MET_sf))
 
 #Graficar
-cols <- c("orange", "darkorange" , 'blue' ,"dark green" ,
-          "green", "seagreen", "darkseagreen", "limegreen", "lemonchiffon" , 'brown', 'red' )
+cols <- c("orange", 
+          "darkorange" , 
+          'blue' ,
+          "dark green" ,
+          "green", 
+          "seagreen", 
+          "darkseagreen", 
+          "limegreen", 
+          "lemonchiffon" , 
+          'brown', 
+          'red' )
 
-tm_shape(MET_sf) + tm_polygons("DN", border.alpha = .5, palette="Reds", legend.show = FALSE,
+tm_shape(MET_sf) + tm_polygons("DN", 
+                               border.alpha = .5, 
+                               palette="Reds", 
+                               legend.show = FALSE,
                                max.categories = 11) + #, palette=cols
-  tm_format("World", title="Cobertura Vegetal y Uso del Suelo \n Linares, Nuevo México") #, col = cols
+  tm_format("World", 
+            title="Cobertura Vegetal y Uso del Suelo \n Linares, Nuevo México") #, col = cols
 
 
 #Union puntos de validación y clasificación
-DatosVal <- st_join(PuntosVal, MET_sf, join = st_intersects) %>%
-  select(c("class", "DN")) %>% na.omit()
+DatosVal <- st_join(PuntosVal, 
+                    MET_sf, 
+                    join = st_intersects) %>%
+            select(c("class", "DN")) %>% 
+            na.omit()
 
-matrizConfucion <- data.frame(matrix(table(DatosVal$class,DatosVal$DN), nrow = 11, ncol = 11))
+matrizConfucion <- data.frame(matrix(table(DatosVal$class,DatosVal$DN), 
+                                     nrow = 11, 
+                                     ncol = 11))
 
 #Clasificación
 Clasificacion <- c("Agricultura I",
