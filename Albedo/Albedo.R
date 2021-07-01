5af0b134b3fbfa3be8ec2b470405b18bbebc9dfd
Hondunauta <klauswiese@users.noreply.github.com>
2021-06-30 20:05:46 -0600

    Update Albedo.R

--- Albedo/Albedo.R
+++ Albedo/Albedo.R
@@ -22,22 +22,34 @@ l <- ReadLandsat8(product)
 
 #Radiance
 ##########
-radiance.blue <- ToTOARadiance(landsat8=l, band="blue")
-radiance.green <- ToTOARadiance(landsat8=l, band="green")
-radiance.red <- ToTOARadiance(landsat8=l, band="red")
-radiance.nir <- ToTOARadiance(landsat8=l, band="nir")
-radiance.swir1 <- ToTOARadiance(landsat8=l, band="swir1")
-radiance.swir2 <- ToTOARadiance(landsat8=l, band="swir2")
+radiance.blue <- ToTOARadiance(landsat8=l,
+                               band="blue")
+radiance.green <- ToTOARadiance(landsat8=l, 
+                                band="green")
+radiance.red <- ToTOARadiance(landsat8=l, 
+                              band="red")
+radiance.nir <- ToTOARadiance(landsat8=l, 
+                              band="nir")
+radiance.swir1 <- ToTOARadiance(landsat8=l, 
+                                band="swir1")
+radiance.swir2 <- ToTOARadiance(landsat8=l, 
+                                band="swir2")
 
 
 #Reflectance
 ############
-reflectance.blue <- ToTOAReflectance(landsat8=l, band="blue")
-reflectance.green <- ToTOAReflectance(landsat8=l, band="green")
-reflectance.red <- ToTOAReflectance(landsat8=l, band="red")
-reflectance.nir <- ToTOAReflectance(landsat8=l, band="nir")
-reflectance.swir1 <- ToTOAReflectance(landsat8=l, band="swir1")
-reflectance.swir2 <- ToTOAReflectance(landsat8=l, band="swir2")
+reflectance.blue <- ToTOAReflectance(landsat8=l, 
+                                     band="blue")
+reflectance.green <- ToTOAReflectance(landsat8=l, 
+                                      band="green")
+reflectance.red <- ToTOAReflectance(landsat8=l, 
+                                    band="red")
+reflectance.nir <- ToTOAReflectance(landsat8=l,
+                                    band="nir")
+reflectance.swir1 <- ToTOAReflectance(landsat8=l, 
+                                      band="swir1")
+reflectance.swir2 <- ToTOAReflectance(landsat8=l, 
+                                      band="swir2")
 
 #Pesos para el cálculo del albedo
 #################################
@@ -53,11 +65,16 @@ SWIR2 <- 0.012
 
 #Cálculo de ALBEDO
 ##################
-Albedo <- (Blue*reflectance.blue + Green*reflectance.green + Red*reflectance.red + NIR*reflectance.nir +
-  SWIR1*reflectance.swir1 + SWIR2*reflectance.swir2)/sum(Blue, Green, Red, NIR, SWIR1, SWIR2)
+Albedo <- (Blue*reflectance.blue + 
+           Green*reflectance.green + 
+           Red*reflectance.red + 
+           NIR*reflectance.nir +
+           SWIR1*reflectance.swir1 + 
+           SWIR2*reflectance.swir2)/sum(Blue, Green, Red, NIR, SWIR1, SWIR2)
 
 #Albedo PNLT
-AlbedoPNLT <- crop(Albedo, extent(PNLT))
+AlbedoPNLT <- crop(Albedo, 
+                   extent(PNLT))
 
 plot(AlbedoPNLT)
 plot(PNLT, add=TRUE)
